35aa4aa93047d8c73ebbd70205ae7f7cf820820a
DBotThePony <dbotthepony@yandex.ru>
2022-05-21 20:45:54 +0700

    Удаление добавленных приборов в новом акте свайпом

--- app/src/main/java/com/example/energo_monitoring/compose/screens/creatingNew3/CreatingNew3Content.kt
+++ app/src/main/java/com/example/energo_monitoring/compose/screens/creatingNew3/CreatingNew3Content.kt
@@ -4,6 +4,7 @@
 package com.example.energo_monitoring.compose.screens.creatingNew3
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -11,16 +12,20 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.energo_monitoring.R
 import com.example.energo_monitoring.compose.screens.creatingNew1.ChooseFromListDialog
 import com.example.energo_monitoring.compose.viewmodels.ClientInfoViewModel
+import java.lang.ref.WeakReference
 import java.math.BigDecimal
+import kotlin.math.roundToInt
 
 private val deviceTypes: List<IDeviceInfo<*>> = listOf(
     HeatCalculator.Companion,
@@ -30,6 +35,7 @@ private val deviceTypes: List<IDeviceInfo<*>> = listOf(
     Counter.Companion,
 )
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun CreatingNew3Content(
     viewModel: ClientInfoViewModel,
@@ -55,6 +61,10 @@ fun CreatingNew3Content(
             fontSize = 17.sp
         )
 
+        val swipeStates = remember {
+            mutableStateListOf<Pair<WeakReference<AbstractDevice<*>>, SwipeableState<Int>>>()
+        }
+
         Column(
             modifier = Modifier
                 // .fillMaxSize()
@@ -63,7 +73,28 @@ fun CreatingNew3Content(
                 .padding(top = 20.dp, start = 15.dp, end = 15.dp)
                 .verticalScroll(rememberScrollState())
         ) {
-            for (device in viewModel.devices) {
+            val sizePx = with(LocalDensity.current) { (-500).dp.toPx() }
+            val anchors = mapOf(sizePx to 1, 0f to 0)
+            var change by remember {
+                mutableStateOf(0)
+            }
+
+            val iterator = viewModel.devices.iterator()
+
+            for (device in iterator) {
+                var findSwipe = swipeStates.firstOrNull { it.first.get() === device }?.second
+
+                if (findSwipe == null) {
+                    findSwipe = rememberSwipeableState(initialValue = 0)
+                    swipeStates.add(WeakReference(device) to findSwipe)
+                }
+
+                if (findSwipe.currentValue == 1) {
+                    iterator.remove()
+                    change++
+                    continue
+                }
+
                 Row(
                     verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
@@ -75,6 +106,12 @@ fun CreatingNew3Content(
 
                             navController?.navigate("create_new_3_device")
                         }
+                        .swipeable(
+                            state = findSwipe,
+                            anchors = anchors,
+                            orientation = Orientation.Horizontal
+                        )
+                        .offset { IntOffset(findSwipe.offset.value.roundToInt(), 0) }
                 ) {
                     Icon(
                         painter = painterResource(id = R.drawable.ic_baseline_device_hub_24),
