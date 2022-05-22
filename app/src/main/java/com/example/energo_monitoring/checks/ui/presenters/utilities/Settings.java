a56fd79a3175ea4495f89447663030c1cf89a619
al.kropotov <al.kropotov@tensor.ru>
2022-05-22 19:40:07 +0700

    Забыл поменять локальный ip на глобальный

--- app/src/main/java/com/example/energo_monitoring/checks/ui/presenters/utilities/Settings.java
+++ app/src/main/java/com/example/energo_monitoring/checks/ui/presenters/utilities/Settings.java
@@ -1,6 +1,6 @@
 package com.example.energo_monitoring.checks.ui.presenters.utilities;
 
 public class Settings {
-    public static String server_ip   = "172.16.0.4"; // локальный: 172.16.0.4, сервер: 62.231.175.113
+    public static String server_ip   = "62.231.175.113"; // локальный: 172.16.0.4, сервер: 62.231.175.113
     public static String server_port = "8080";
 }
